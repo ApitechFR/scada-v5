be52fd7134f76d887f1f6a4bb786ba342dc0a8c9
CamoinLancelot <kkamyz6@gmail.com>
2023-06-14 14:11:49 +0200

    Feat: ComponentGroupe

---
+++ ScadaWeb/ScadaScheme/ScadaSchemeCommon/Model/ComponentGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scada.Scheme.Model
+{
+    /// <summary>
+    /// Scheme that represent a group of components
+    /// </summary>
+    [Serializable]
+    internal class ComponentGroup : BaseComponent
+    {
+        public ComponentGroup() :base() { }
+    }
+}
