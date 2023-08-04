b2dadbc4079718369682cb506593fce32825550d
Anaïs THORAL <50917972+anaisthrl@users.noreply.github.com>
2023-08-04 15:32:31 +0200

    Update FrmAlias.cs

--- ScadaWeb/ScadaScheme/ScadaSchemeEditor/FrmAlias.cs
+++ ScadaWeb/ScadaScheme/ScadaSchemeEditor/FrmAlias.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Scada.Scheme.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,35 @@ namespace Scada.Scheme.Editor
 {
     public partial class FrmAlias : Form
     {
+        //todo: à supprimer
+        Symbol s = new Symbol();
+
         public FrmAlias()
         {
             InitializeComponent();
+
+            AddAliasAndSymbole();
+
+            FillListBox();
+        }
+
+        //todo: à supprimer
+        private void AddAliasAndSymbole()
+        {
+            Alias al = new Alias();
+            al.Name = "alias test";
+            al.AliasType = typeof(string);
+            al.Value = "value test";
+            al.isCnlLinked = false;
+
+            s.Name = "name symbole test";
+            s.AliasList.Add(al);
+        }
+
+        private void FillListBox()
+        {
+            foreach (var a in s.AliasList)
+                listBox1.Items.Add(a.Name);
         }
     }
 }
