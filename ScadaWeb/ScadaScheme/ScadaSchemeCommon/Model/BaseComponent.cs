f3d150b8e2d129b542dd165bc48e67155213507b
Anaïs THORAL <50917972+anaisthrl@users.noreply.github.com>
2023-09-25 16:06:55 +0200

    feat: update save symbols

--- ScadaWeb/ScadaScheme/ScadaSchemeCommon/Model/BaseComponent.cs
+++ ScadaWeb/ScadaScheme/ScadaSchemeCommon/Model/BaseComponent.cs
@@ -309,10 +309,10 @@ namespace Scada.Scheme.Model
             xmlElem.AppendElem("ZIndex", ZIndex);
             xmlElem.AppendElem("GroupID", GroupId);
 
-            XmlElement aliasListElem = xmlElem.SelectSingleNode("AliasList") as XmlElement;
+            XmlElement aliasListElem = xmlElem.SelectSingleNode("AliasListOfComponent") as XmlElement;
             if (aliasListElem == null)
             {
-                aliasListElem = xmlElem.OwnerDocument.CreateElement("AliasList");
+                aliasListElem = xmlElem.OwnerDocument.CreateElement("AliasListOfComponent");
                 xmlElem.AppendChild(aliasListElem);
             }
 
@@ -321,12 +321,12 @@ namespace Scada.Scheme.Model
                 string aliasName = kvp.Value.Name;
 
                 bool aliasExists = aliasListElem
-                    .SelectNodes($"Alias[AliasName='{aliasName}']")
+                    .SelectNodes($"AliasOfComponent[AliasName='{aliasName}']")
                     .Count > 0;
 
                 if (!aliasExists)
                 {
-                    XmlElement aliasElem = xmlElem.OwnerDocument.CreateElement("Alias");
+                    XmlElement aliasElem = xmlElem.OwnerDocument.CreateElement("AliasOfComponent");
                     aliasElem.AppendElem("AliasName", aliasName);
                     aliasElem.AppendElem("AttributeName", kvp.Key.ToString());
                     aliasListElem.AppendChild(aliasElem);
