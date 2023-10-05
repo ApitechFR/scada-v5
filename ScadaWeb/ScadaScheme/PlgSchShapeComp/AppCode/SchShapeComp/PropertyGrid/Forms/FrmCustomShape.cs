70149ed27ad4747f78bbe7d866f5793e02583d8a
MG messie <moukmessie@gmail.com>
2023-10-05 09:37:39 +0200

    feat: upload svg file

--- ScadaWeb/ScadaScheme/PlgSchShapeComp/AppCode/SchShapeComp/PropertyGrid/Forms/FrmCustomShape.cs
+++ ScadaWeb/ScadaScheme/PlgSchShapeComp/AppCode/SchShapeComp/PropertyGrid/Forms/FrmCustomShape.cs
@@ -59,6 +59,68 @@ namespace Scada.Web.Plugins.SchShapeComp.PropertyGrid
 			}
 		}
 
+		//private void BtnEditExternally_Click(object sender, EventArgs e)
+		//{
+		//	if (IsExternalEditorOpen)
+		//	{
+		//		BtnDoneEditing_Click(sender, e);
+		//		return;
+		//	}
+
+		//	try
+		//	{
+		//		File.WriteAllText(tempFilePath, webBrowser1.DocumentText);
+		//		string editorPath = Properties.Settings.Default.EditorPath;
+
+		//		if (string.IsNullOrEmpty(editorPath))
+		//		{
+		//			DialogResult result = MessageBox.Show("Voulez-vous sélectionner un éditeur personnalisé ou utiliser l'éditeur par défaut?",
+		//												  "Choix de l'éditeur",
+		//												  MessageBoxButtons.YesNoCancel,
+		//												  MessageBoxIcon.Question);
+
+		//			if (result == DialogResult.Yes)
+		//			{
+		//				using (OpenFileDialog openFileDialog = new OpenFileDialog())
+		//				{
+		//					openFileDialog.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
+		//					if (openFileDialog.ShowDialog() == DialogResult.OK)
+		//					{
+		//						editorPath = openFileDialog.FileName;
+		//						Properties.Settings.Default.EditorPath = editorPath;
+		//						Properties.Settings.Default.Save();
+		//					}
+		//					else
+		//					{
+		//						return;
+		//					}
+		//				}
+		//			}
+		//			else if (result == DialogResult.No)
+		//			{
+		//				externalEditorProcess = Process.Start(tempFilePath);
+		//				WatchFileChanges();
+		//				IsExternalEditorOpen = true;
+		//				((Button)sender).Text = "Done";
+		//				return;
+		//			}
+		//			else
+		//			{
+		//				return;
+		//			}
+		//		}
+
+		//		externalEditorProcess = Process.Start(editorPath, tempFilePath);
+		//		WatchFileChanges();
+		//		IsExternalEditorOpen = true;
+		//		((Button)sender).Text = "Done";
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		MessageBox.Show($"Failed to open the SVG in the external editor: {ex.Message}");
+		//		Console.WriteLine(ex.Message);
+		//	}
+		//}
 		private void BtnEditExternally_Click(object sender, EventArgs e)
 		{
 			if (IsExternalEditorOpen)
@@ -70,47 +132,9 @@ namespace Scada.Web.Plugins.SchShapeComp.PropertyGrid
 			try
 			{
 				File.WriteAllText(tempFilePath, webBrowser1.DocumentText);
-				string editorPath = Properties.Settings.Default.EditorPath;
 
-				if (string.IsNullOrEmpty(editorPath))
-				{
-					DialogResult result = MessageBox.Show("Voulez-vous sélectionner un éditeur personnalisé ou utiliser l'éditeur par défaut?",
-														  "Choix de l'éditeur",
-														  MessageBoxButtons.YesNoCancel,
-														  MessageBoxIcon.Question);
-
-					if (result == DialogResult.Yes)
-					{
-						using (OpenFileDialog openFileDialog = new OpenFileDialog())
-						{
-							openFileDialog.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
-							if (openFileDialog.ShowDialog() == DialogResult.OK)
-							{
-								editorPath = openFileDialog.FileName;
-								Properties.Settings.Default.EditorPath = editorPath;
-								Properties.Settings.Default.Save();
-							}
-							else
-							{
-								return;
-							}
-						}
-					}
-					else if (result == DialogResult.No)
-					{
-						externalEditorProcess = Process.Start(tempFilePath);
-						WatchFileChanges();
-						IsExternalEditorOpen = true;
-						((Button)sender).Text = "Done";
-						return;
-					}
-					else
-					{
-						return;
-					}
-				}
-
-				externalEditorProcess = Process.Start(editorPath, tempFilePath);
+				// Utiliser l'éditeur par défaut de Windows pour ouvrir le fichier
+				externalEditorProcess = Process.Start(tempFilePath);
 				WatchFileChanges();
 				IsExternalEditorOpen = true;
 				((Button)sender).Text = "Done";
@@ -121,6 +145,7 @@ namespace Scada.Web.Plugins.SchShapeComp.PropertyGrid
 				Console.WriteLine(ex.Message);
 			}
 		}
+
 
 		private void WatchFileChanges()
 		{
