eeeee9b8997a3c4e8ddf7193bbd9d8e47338075c
MG messie <moukmessie@gmail.com>
2023-11-03 14:21:50 +0100

    feat: Implement SVGTypeConverter for SVG code property display

---
+++ ScadaWeb/ScadaScheme/PlgSchShapeComp/AppCode/SchShapeComp/PropertyGrid/SVGTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+
+namespace Scada.Web.Plugins.SchShapeComp.PropertyGrid
+{
+	public class SVGTypeConverter : TypeConverter
+	{
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if (destinationType == typeof(string))
+			{
+				string stringValue = value as string;
+				if (!string.IsNullOrEmpty(stringValue))
+				{
+					return stringValue.Length > 5 ? $"{stringValue.Substring(0, 20)}..." : stringValue;
+				}
+				else
+				{
+					return "Import SVG file";
+				}
+			}
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+	}
+
+}
