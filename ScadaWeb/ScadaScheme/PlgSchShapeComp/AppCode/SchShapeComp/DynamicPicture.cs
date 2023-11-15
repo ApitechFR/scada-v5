deb835d126157b325387186ee3c59bed8003020d
MG messie <moukmessie@gmail.com>
2023-11-15 16:32:46 +0100

    add dynamic picture and text

---
+++ ScadaWeb/ScadaScheme/PlgSchShapeComp/AppCode/SchShapeComp/DynamicPicture.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Xml;
+using Scada.Scheme.Model;
+using System.Drawing.Design;
+using CM = System.ComponentModel;
+using System.Collections.Generic;
+using Scada.Scheme.Model.DataTypes;
+using Scada.Scheme.Model.PropertyGrid;
+using Scada.Web.Plugins.SchShapeComp.PropertyGrid;
+
+namespace Scada.Web.Plugins.SchShapeComp
+{
+	[Serializable]
+	public class DynamicPicture : StaticPicture, IDynamicComponent
+	{
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		public DynamicPicture()
+			: base()
+		{
+			BackColorOnHover = "";
+			BorderColorOnHover = "";
+			ImageOnHoverName = "";
+			Action = Actions.None;
+			Conditions = new List<PictureCondition>();
+			InCnlNum = 0;
+			CtrlCnlNum = 0;
+			InCnlNumCustom = "(0) NA";
+			CtrlCnlNumCustom = "(0) NA";
+		}
+
+
+		/// <summary>
+		/// Получить или установить цвет фона при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Back color on hover"), Category(Categories.Behavior)]
+		[Description("The background color of the component when user rests the pointer on it.")]
+		[CM.Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+		#endregion
+		public string BackColorOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить цвет рамки при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Border color on hover"), Category(Categories.Behavior)]
+		[Description("The border color of the component when user rests the pointer on it.")]
+		[CM.Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+		#endregion
+		public string BorderColorOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить наименование изображения, отображаемого при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Image on hover"), Category(Categories.Behavior)]
+		[Description("The image shown when user rests the pointer on the component.")]
+		[CM.TypeConverter(typeof(ImageConverter)), CM.Editor(typeof(ImageEditor), typeof(UITypeEditor))]
+		[CM.DefaultValue("")]
+		#endregion
+		public string ImageOnHoverName { get; set; }
+
+		/// <summary>
+		/// Получить или установить действие
+		/// </summary>
+		#region Attributes
+		[DisplayName("Action"), Category(Categories.Behavior)]
+		[Description("The action executed by clicking the left mouse button on the component.")]
+		[CM.DefaultValue(Actions.None)]
+		#endregion
+		public Actions Action { get; set; }
+
+		/// <summary>
+		/// Получить условия вывода изображений
+		/// </summary>
+		#region Attributes
+		[DisplayName("Conditions"), Category(Categories.Behavior)]
+		[Description("The conditions for image output depending on the value of the input channel.")]
+		[CM.DefaultValue(null), CM.TypeConverter(typeof(CollectionConverter))]
+		[CM.Editor(typeof(CollectionEditor), typeof(UITypeEditor))]
+		#endregion
+		public List<PictureCondition> Conditions { get; protected set; }
+
+		/// <summary>
+		/// Получить или установить номер входного канала
+		/// </summary>
+		#region Attributes
+		[CM.Browsable(false)]
+		[DisplayName("Input channel"), Category(Categories.Data)]
+		[Description("The input channel number associated with the component.")]
+		[CM.DefaultValue(0)]
+		#endregion
+		public int InCnlNum { get; set; }
+
+		/// <summary>
+		/// Получить или установить номер входного канала
+		/// </summary>
+		#region Attributes
+		[DisplayName("Input channel"), Category(Categories.Data)]
+		[Description("The input channel number associated with the component.")]
+		[CM.Editor(typeof(IDcustomEditor), typeof(UITypeEditor))]
+		#endregion
+		public string InCnlNumCustom { get; set; }
+
+		/// <summary>
+		/// Получить или установить номер канала управления
+		/// </summary>
+		#region Attributes
+		[CM.Browsable(false)]
+		[DisplayName("Output channel"), Category(Categories.Data)]
+		[Description("The output channel number associated with the component.")]
+		[CM.DefaultValue(0)]
+		#endregion
+		public int CtrlCnlNum { get; set; }
+
+
+		/// <summary>
+		/// Получить или установить номер канала управления
+		/// </summary>
+		#region Attributes
+		[DisplayName("Output channel"), Category(Categories.Data)]
+		[Description("The output channel number associated with the component.")]
+		[CM.Editor(typeof(IDcustomEditor), typeof(UITypeEditor))]
+		#endregion
+		public string CtrlCnlNumCustom { get; set; }
+
+		/// <summary>
+		/// Загрузить конфигурацию компонента из XML-узла
+		/// </summary>
+		public override void LoadFromXml(XmlNode xmlNode)
+		{
+			base.LoadFromXml(xmlNode);
+
+			BackColorOnHover = xmlNode.GetChildAsString("BackColorOnHover");
+			BorderColorOnHover = xmlNode.GetChildAsString("BorderColorOnHover");
+			ImageOnHoverName = xmlNode.GetChildAsString("ImageOnHoverName");
+			Action = xmlNode.GetChildAsEnum<Actions>("Action");
+
+			XmlNode conditionsNode = xmlNode.SelectSingleNode("Conditions");
+			if (conditionsNode != null)
+			{
+				Conditions = new List<PictureCondition>();
+				XmlNodeList conditionNodes = conditionsNode.SelectNodes("Condition");
+				foreach (XmlNode conditionNode in conditionNodes)
+				{
+					PictureCondition condition = new PictureCondition { SchemeView = SchemeView };
+					condition.LoadFromXml(conditionNode);
+					Conditions.Add(condition);
+				}
+			}
+
+			InCnlNum = xmlNode.GetChildAsInt("InCnlNum");
+			CtrlCnlNum = xmlNode.GetChildAsInt("CtrlCnlNum");
+			InCnlNumCustom = xmlNode.GetChildAsString("InCnlNumCustom");
+			CtrlCnlNumCustom = xmlNode.GetChildAsString("CtrlCnlNumCustom");
+		}
+
+		/// <summary>
+		/// Сохранить конфигурацию компонента в XML-узле
+		/// </summary>
+		public override void SaveToXml(XmlElement xmlElem)
+		{
+			base.SaveToXml(xmlElem);
+
+			xmlElem.AppendElem("BackColorOnHover", BackColorOnHover);
+			xmlElem.AppendElem("BorderColorOnHover", BorderColorOnHover);
+			xmlElem.AppendElem("ImageOnHoverName", ImageOnHoverName);
+			xmlElem.AppendElem("Action", Action);
+
+			XmlElement conditionsElem = xmlElem.AppendElem("Conditions");
+			foreach (Condition condition in Conditions)
+			{
+				XmlElement conditionElem = conditionsElem.AppendElem("Condition");
+				condition.SaveToXml(conditionElem);
+			}
+
+			xmlElem.AppendElem("InCnlNum", InCnlNum);
+			xmlElem.AppendElem("CtrlCnlNum", CtrlCnlNum);
+			xmlElem.AppendElem("CtrlCnlNumCustom", CtrlCnlNumCustom);
+			xmlElem.AppendElem("InCnlNumCustom", InCnlNumCustom);
+		}
+
+		/// <summary>
+		/// Клонировать объект
+		/// </summary>
+		public override BaseComponent Clone()
+		{
+			DynamicPicture clonedComponent = (DynamicPicture)base.Clone();
+
+			foreach (Condition condition in clonedComponent.Conditions)
+			{
+				condition.SchemeView = SchemeView;
+			}
+
+			return clonedComponent;
+		}
+	}
+}
---
+++ ScadaWeb/ScadaScheme/PlgSchShapeComp/AppCode/SchShapeComp/DynamicText.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Xml;
+using Scada.Scheme.Model;
+using System.Drawing.Design;
+using CM = System.ComponentModel;
+using Scada.Scheme.Model.DataTypes;
+using Scada.Scheme.Model.PropertyGrid;
+
+namespace Scada.Web.Plugins.SchShapeComp
+{
+	[Serializable]
+	public class DynamicText : StaticText, IDynamicComponent
+	{
+		/// <summary>
+		/// Текст надписи по умолчанию
+		/// </summary>
+		new public static readonly string DefaultText =
+			Localization.UseRussian ? "Динамическая надпись" : "Dynamic text";
+
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		public DynamicText()
+			: base()
+		{
+			Text = DefaultText;
+			BackColorOnHover = "";
+			BorderColorOnHover = "";
+			ForeColorOnHover = "";
+			UnderlineOnHover = false;
+			Action = Actions.None;
+			ShowValue = ShowValueKinds.ShowWithUnit;
+			InCnlNum = 0;
+			CtrlCnlNum = 0;
+			InCnlNumCustom = "(0) NA";
+			CtrlCnlNumCustom = "(0) NA";
+		}
+
+
+		/// <summary>
+		/// Получить или установить цвет фона при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Back color on hover"), Category(Categories.Behavior)]
+		[Description("The background color of the component when user rests the pointer on it.")]
+		[CM.Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+		#endregion
+		public string BackColorOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить цвет рамки при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Border color on hover"), Category(Categories.Behavior)]
+		[Description("The border color of the component when user rests the pointer on it.")]
+		[CM.Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+		#endregion
+		public string BorderColorOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить цвет текста при наведени указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Fore color on hover"), Category(Categories.Behavior)]
+		[Description("The foreground color of the component, which is used to display text, when user rests the pointer on it.")]
+		[CM.Editor(typeof(ColorEditor), typeof(UITypeEditor))]
+		#endregion
+		public string ForeColorOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить признак подчёркивания при наведении указателя мыши
+		/// </summary>
+		#region Attributes
+		[DisplayName("Underline on hover"), Category(Categories.Behavior)]
+		[Description("Underline text when user rests the pointer on the component.")]
+		[CM.DefaultValue(false), CM.TypeConverter(typeof(BooleanConverter))]
+		#endregion
+		public bool UnderlineOnHover { get; set; }
+
+		/// <summary>
+		/// Получить или установить действие
+		/// </summary>
+		#region Attributes
+		[DisplayName("Action"), Category(Categories.Behavior)]
+		[Description("The action executed by clicking the left mouse button on the component.")]
+		[CM.DefaultValue(Actions.None)]
+		#endregion
+		public Actions Action { get; set; }
+
+		/// <summary>
+		/// Получить или установить признак вывода значения входного канала
+		/// </summary>
+		#region Attributes
+		[DisplayName("Show value"), Category(Categories.Behavior)]
+		[Description("Show a value of the input channel associated with the component.")]
+		[CM.DefaultValue(ShowValueKinds.ShowWithUnit)]
+		#endregion
+		public ShowValueKinds ShowValue { get; set; }
+
+		/// <summary>
+		/// Получить или установить номер входного канала
+		/// </summary>
+		#region Attributes
+		[CM.Browsable(false)]
+		[DisplayName("Input channel"), Category(Categories.Data)]
+		[Description("The input channel number associated with the component.")]
+		[CM.DefaultValue(0)]
+		#endregion
+		public int InCnlNum { get; set; }
+
+
+		/// <summary>
+		/// Получить или установить номер входного канала
+		/// </summary>
+		#region Attributes
+		[DisplayName("Input channel"), Category(Categories.Data)]
+		[Description("The input channel number associated with the component.")]
+		[CM.Editor(typeof(IDcustomEditor), typeof(UITypeEditor))]
+		#endregion
+		public string InCnlNumCustom { get; set; }
+
+
+		/// <summary>
+		/// Получить или установить номер канала управления
+		/// </summary>
+		#region Attributes
+		[DisplayName("Output channel"), Category(Categories.Data)]
+		[Description("The output channel number associated with the component.")]
+		[CM.Editor(typeof(IDcustomEditor), typeof(UITypeEditor))]
+		#endregion
+		public string CtrlCnlNumCustom { get; set; }
+
+		/// <summary>
+		/// Получить или установить номер канала управления
+		/// </summary>
+		#region Attributes
+		[CM.Browsable(false)]
+		[DisplayName("Output channel"), Category(Categories.Data)]
+		[Description("The output channel number associated with the component.")]
+		[CM.DefaultValue(0)]
+		#endregion
+		public int CtrlCnlNum { get; set; }
+
+
+		/// <summary>
+		/// Загрузить конфигурацию компонента из XML-узла
+		/// </summary>
+		public override void LoadFromXml(XmlNode xmlNode)
+		{
+			base.LoadFromXml(xmlNode);
+
+			BackColorOnHover = xmlNode.GetChildAsString("BackColorOnHover");
+			BorderColorOnHover = xmlNode.GetChildAsString("BorderColorOnHover");
+			ForeColorOnHover = xmlNode.GetChildAsString("ForeColorOnHover");
+			UnderlineOnHover = xmlNode.GetChildAsBool("UnderlineOnHover");
+			Action = xmlNode.GetChildAsEnum<Actions>("Action");
+			ShowValue = xmlNode.GetChildAsEnum<ShowValueKinds>("ShowValue");
+			InCnlNum = xmlNode.GetChildAsInt("InCnlNum");
+			CtrlCnlNum = xmlNode.GetChildAsInt("CtrlCnlNum");
+			InCnlNumCustom = xmlNode.GetChildAsString("InCnlNumCustom");
+			CtrlCnlNumCustom = xmlNode.GetChildAsString("CtrlCnlNumCustom");
+		}
+
+		/// <summary>
+		/// Сохранить конфигурацию компонента в XML-узле
+		/// </summary>
+		public override void SaveToXml(XmlElement xmlElem)
+		{
+			base.SaveToXml(xmlElem);
+
+			xmlElem.AppendElem("BackColorOnHover", BackColorOnHover);
+			xmlElem.AppendElem("BorderColorOnHover", BorderColorOnHover);
+			xmlElem.AppendElem("ForeColorOnHover", ForeColorOnHover);
+			xmlElem.AppendElem("UnderlineOnHover", UnderlineOnHover);
+			xmlElem.AppendElem("Action", Action);
+			xmlElem.AppendElem("ShowValue", ShowValue);
+			xmlElem.AppendElem("InCnlNum", InCnlNum);
+			xmlElem.AppendElem("CtrlCnlNum", CtrlCnlNum);
+			xmlElem.AppendElem("CtrlCnlNumCustom", CtrlCnlNumCustom);
+			xmlElem.AppendElem("InCnlNumCustom", InCnlNumCustom);
+		}
+	}
+
+}
