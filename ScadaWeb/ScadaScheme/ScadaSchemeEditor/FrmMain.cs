1821b72dc7502a7498dbe19f5d2db3cfcbc03f4d
Louis Corbet <ls.corbet@gmail.com>
2024-01-16 17:11:17 +0100

    fix: update treeview on symbol conversion

--- ScadaWeb/ScadaScheme/ScadaSchemeEditor/FrmMain.cs
+++ ScadaWeb/ScadaScheme/ScadaSchemeEditor/FrmMain.cs
@@ -2223,6 +2223,7 @@ namespace Scada.Scheme.Editor
 
                 foreach (BaseComponent child in childrenToChange)
                 {
+					child.ItemChanged += Scheme_ItemChanged;
                     child.GroupId = (c.GroupId != -1) ? c.GroupId : -1;
                     child.AliasesDictionnary = new Dictionary<string, Alias>();
                     child.OnItemChanged(SchemeChangeTypes.ComponentChanged, child);
